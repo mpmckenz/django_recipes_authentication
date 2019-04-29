af2fafd70ffb9b311124a4a3e52d7a19440649fe
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-29 17:04:03 -0400

    both models created

--- backend_django_recipes/models.py
+++ backend_django_recipes/models.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
     def __str__(self):
         return self.name
-    pass
 
 
 class Recipes(models.Model):
-    pass
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.CharField(max_length=140)
+    time_req = models.CharField(max_length=25)
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
