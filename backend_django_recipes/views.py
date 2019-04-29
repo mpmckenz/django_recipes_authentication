8bfbaa6049d6805340bf177e170d08f50aefd740
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-29 17:04:29 -0400

    views updated but not complete

--- backend_django_recipes/views.py
+++ backend_django_recipes/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render
 # ^^^ djnago has lots of imports and packages, so collect most common and put in shortcuts
 from backend_django_recipes.models import Recipes
 
-# doenst have to be called index
 
-
-def index(request):
-    html = "index.html"
+def list_view(request):
+    html = "list_view.html"
     items = Recipes.objects.all()
-    return render(request, html, {"stuff", })
-    pass
+    return render(request, html, {"stuff": items})
