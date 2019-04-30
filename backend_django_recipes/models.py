c8e4bc8255d2cc313cf0862fa823a008fd2586b1
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-30 15:44:56 -0400

    model outline done

--- backend_django_recipes/models.py
+++ backend_django_recipes/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
