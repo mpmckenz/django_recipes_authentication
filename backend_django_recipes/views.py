ae729a9900509f8bfbc2e74e2c3e410cf9c4702c
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-30 15:46:11 -0400

    views proper

--- backend_django_recipes/views.py
+++ backend_django_recipes/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 # ^^^ djnago has lots of imports and packages, so collect most common and put in shortcuts
-from backend_django_recipes.models import Recipes
+from backend_django_recipes.models import Recipes, Author
 
 
 def list_view(request):
     html = "list_view.html"
-    items = Recipes.objects.all()
-    return render(request, html, {"stuff": items})
+    items = Recipes.objects.all().order_by("title")
+    return render(request, html, {"list": items})
+
+
+def recipe_detail(request, id):
+    html = "recipe_detail.html"
+    items = Recipes.objects.all().filter(id=id)
+    return render(request, html, {"recipes": items})
+
+
+def author_detail(request, id):
+    html = "author_detail.html"
+    authors = Author.objects.all().filter(id=id)
+    items = Recipes.objects.all().filter(author_id=id)
+    return render(request, html, {"authors": authors, "recipes": items})
