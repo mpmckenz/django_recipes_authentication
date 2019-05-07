e5dda593877acfe0fc21d05a9f827a26c5c7d84e
Michelle Hoelzer <hoelzermm@gmail.com>
2019-05-07 15:05:51 -0400

    size of form boxes made

--- backend_django_recipes/forms.py
+++ backend_django_recipes/forms.py
@@ -5,7 +5,7 @@ from backend_django_recipes.models import Author
 class AuthorsForm(forms.Form):
     name = forms.CharField(max_length=500)
     username = forms.CharField(max_length=50)
-    bio = forms.CharField(widget=forms.Textarea)
+    bio = forms.CharField(widget=forms.Textarea(attrs={"rows":"3"}))
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
 
@@ -15,15 +15,7 @@ class RecipesForm(forms.Form):
     author = forms.ModelChoiceField(queryset=Author.objects.all())
     description = forms.CharField(max_length=140)
     time_req = forms.CharField(max_length=25)
-    instructions = forms.CharField(widget=forms.Textarea)
-
-
-# class AddAuthorForm(forms.Form):
-#     name = forms.CharField(max_length=500)
-#     username = forms.CharField(max_length=50)
-#     bio = forms.CharField(widget=forms.Textarea)
-#     email = forms.EmailField()
-#     password = forms.CharField(widget=forms.PasswordInput())
+    instructions = forms.CharField(widget=forms.Textarea(attrs={"rows":"3"}))
 
 
 class LoginForm(forms.Form):
